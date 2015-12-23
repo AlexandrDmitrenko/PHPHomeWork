0ff1576578d90e8d1511349bb11e38c97018647e
Sanja <vs_sanja@mail.ru>
2015-12-23 22:48:32 +0200

    com

---
+++ lesson2/index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Lesson 2</title>
+</head>
+<body>
+<?php
+
+    $name = "Александр";
+    $age = 33;
+    echo "Меня зовут $name <br>";
+    echo "Мне $age года";
+echo "<br>","----------------------------------------------------------------------","<br>";
+
+    $a = 9;
+    $s = 1/4*($a * sqrt(3) );
+    echo $s;
+
+?>
+
+</body>
+</html>
